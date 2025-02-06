db391d72fdf97152dc690b5d149d510b2392d29e
xisanlou <xisanlou@example.com>
2025-02-06 10:59:54 +0800

    complete30

--- exercises/hashmaps/hashmaps3.rs
+++ exercises/hashmaps/hashmaps3.rs
@@ -14,7 +14,7 @@
 // Execute `rustlings hint hashmaps3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 use std::collections::HashMap;
 
@@ -39,6 +39,19 @@ fn build_scores_table(results: String) -> HashMap<String, Team> {
         // will be the number of goals conceded from team_2, and similarly
         // goals scored by team_2 will be the number of goals conceded by
         // team_1.
+        if let Some(x) = scores.get_mut(&team_1_name) {
+            x.goals_scored += team_1_score;
+            x.goals_conceded += team_2_score;
+        } else {
+            scores.insert(team_1_name.clone(), Team{goals_scored: team_1_score, goals_conceded: team_2_score});
+        }
+
+        if let Some(x) = scores.get_mut(&team_2_name) {
+            x.goals_scored += team_2_score;
+            x.goals_conceded += team_1_score;
+        } else {
+            scores.insert(team_2_name.clone(), Team{goals_scored: team_2_score, goals_conceded: team_1_score});
+        }
     }
     scores
 }
--- exercises/quiz2.rs
+++ exercises/quiz2.rs
@@ -20,7 +20,7 @@
 //
 // No hints this time!
 
-// I AM NOT DONE
+
 
 pub enum Command {
     Uppercase,
@@ -32,11 +32,17 @@ mod my_module {
     use super::Command;
 
     // TODO: Complete the function signature!
-    pub fn transformer(input: ???) -> ??? {
+    pub fn transformer(input: Vec<(String, Command)>) -> Vec<String> {
         // TODO: Complete the output declaration!
-        let mut output: ??? = vec![];
+        let mut output = vec![];
         for (string, command) in input.iter() {
             // TODO: Complete the function body. You can do it!
+            match command {
+                Command::Uppercase => {output.push(string.to_uppercase())},
+                Command::Trim => {output.push(string.trim().to_string())},
+                Command::Append(x) => {output.push(string.clone() + &"bar".repeat(*x))},
+                _ => continue,
+            }
         }
         output
     }
@@ -45,7 +51,7 @@ mod my_module {
 #[cfg(test)]
 mod tests {
     // TODO: What do we need to import to have `transformer` in scope?
-    use ???;
+    use super::my_module::transformer;
     use super::Command;
 
     #[test]
